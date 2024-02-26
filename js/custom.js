88c23397f1e6cee8007f805a5652d6aef98c2cfc
unknown <dtf5508@gmail.com>
2024-02-26 17:59:24 +0900

    js제외 완성?

--- js/custom.js
+++ js/custom.js
@@ -20,3 +20,44 @@ function slide() {
 setInterval(function () {
   slide();
 } ,2800);
+
+/* 공지사항 */
+$('.section3_top_left_box_1 a').on('click', function () {
+	/* 제목 색상 다르게 */
+		/* //색상다르게하기 */
+	$('.box1').show();
+	$('.red1').show();
+	$('.box2').hide();
+  $('.red2').hide();
+  $('.red3').hide();
+  $('.box3').hide();
+	return false;
+});
+$('.section3_top_left_box_2 a').on('click', function () {
+	/* 제목 색상 다르게 */
+	/* //색상다르게하기 */
+	$('.box1').hide();
+  $('.red1').hide();
+  $('.red3').hide();
+  $('.box3').hide();
+	$('.box2').show();
+	$('.red2').show();
+	return false;
+});
+$('.section3_top_left_box_3 a').on('click', function () {
+	/* 제목 색상 다르게 */
+		/* //색상다르게하기 */
+	$('.box3').show();
+	$('.red3').show();
+	$('.box2').hide();
+  $('.red2').hide();
+  $('.red1').hide();
+  $('.box1').hide();
+	return false;
+});
+$('.box1').show();
+$('.red1').show();
+$('.box2').hide();
+$('.red2').hide();
+$('.red3').hide();
+$('.box3').hide();
